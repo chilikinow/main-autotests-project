a866fb488dd26ccb280c3af1e38afdf068e3d8a0
Чиликин Олег Владимирович <ChilikinOV@gnivc.ru>
2023-01-13 00:43:11 +0300

    Revert "- HW 10. Jenkins"
    This reverts commit d4e56db0

--- src/test/java/com/company/project/testdata/UITestBase.java
+++ src/test/java/com/company/project/testdata/UITestBase.java
@@ -2,7 +2,6 @@ package com.company.project.testdata;
 
 import com.codeborne.selenide.Configuration;
 import com.company.project.helpers.Attach;
-import com.company.project.pages.TestBase;
 import lombok.extern.slf4j.Slf4j;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
@@ -10,7 +9,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 @Slf4j
-public class UITestBase extends TestBase {
+public class UITestBase {
 
     private static String selenideLocation;
 
