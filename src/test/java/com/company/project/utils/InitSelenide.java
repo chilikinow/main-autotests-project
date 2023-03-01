b729e46581d08bd30dcb4f2d54bcd37e3701a001
Чиликин Олег Владимирович <ChilikinOV@gnivc.ru>
2023-03-01 23:23:17 +0300

    master - add new tests

--- src/test/java/com/company/project/utils/InitSelenide.java
+++ src/test/java/com/company/project/utils/InitSelenide.java
@@ -1,7 +1,10 @@
 package com.company.project.utils;
 
 import com.codeborne.selenide.Configuration;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.FileDownloadMode.FOLDER;
 
 public class InitSelenide {
 
@@ -9,18 +12,18 @@ public class InitSelenide {
 
     static {
 
-//        selenideLocation = System.getProperty("selenide.location");
-//
-//        SelenideConfig selenideConfig = ConfigFactory.create(SelenideConfig.class, System.getProperties());
-//        Configuration.remote = selenideConfig.getSelenoidUrl();
-//        Configuration.browser = selenideConfig.getBrowserName();
-//        Configuration.browserVersion = selenideConfig.getBrowserVersion();
-//        Configuration.browserSize = selenideConfig.getBrowserSize();
-//        Configuration.timeout = selenideConfig.getBrowserTimeout();
-//        Configuration.headless = selenideConfig.isBrowserHeadless();
-//        Configuration.holdBrowserOpen = selenideConfig.isHoldBrowserOpen();
-//
-//        Configuration.fileDownload = FOLDER;
+        selenideLocation = System.getProperty("selenide.location");
+
+        SelenideConfig selenideConfig = ConfigFactory.create(SelenideConfig.class, System.getProperties());
+        Configuration.remote = selenideConfig.getSelenoidUrl();
+        Configuration.browser = selenideConfig.getBrowserName();
+        Configuration.browserVersion = selenideConfig.getBrowserVersion();
+        Configuration.browserSize = selenideConfig.getBrowserSize();
+        Configuration.timeout = selenideConfig.getBrowserTimeout();
+        Configuration.headless = selenideConfig.isBrowserHeadless();
+        Configuration.holdBrowserOpen = selenideConfig.isHoldBrowserOpen();
+
+        Configuration.fileDownload = FOLDER;
 
         if (selenideLocation != null && selenideLocation.equals("remote")) {
 
