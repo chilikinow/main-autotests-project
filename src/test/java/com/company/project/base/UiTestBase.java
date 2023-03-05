a294887bddd0538d0eea798034b87428f0043d51
Чиликин Олег Владимирович <ChilikinOV@gnivc.ru>
2023-03-05 18:22:09 +0300

    master - minor changes

--- src/test/java/com/company/project/base/UiTestBase.java
+++ src/test/java/com/company/project/base/UiTestBase.java
@@ -6,7 +6,6 @@ import com.company.project.utils.InitSelenide;
 import com.company.project.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
